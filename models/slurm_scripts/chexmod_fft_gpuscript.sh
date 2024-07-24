4ea0d3508ac184342add0633713a421e01f886db
macuser <fz221@ic.ac.uk>
2024-07-24 10:25:23 +0100

    Second attempt at changing partition to gpus instead of gpus24 due to resources limitations -- only applied to chexmod_fft_gpuscript.sh for now.

--- models/slurm_scripts/chexmod_fft_gpuscript.sh
+++ models/slurm_scripts/chexmod_fft_gpuscript.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=gpus24       
+#SBATCH --partition=gpus   
 #SBATCH --gres=gpu:1             
 #SBATCH --output=/vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/slurm_scripts/chexmod_fft.%N.%j.log  
-#SBATCH --time=3-00:00:00        
+#SBATCH --time=3-00:00:00
+#SBATCH --nodelist=monal03        
 
 #SBATCH --job-name=chexmod_fft
 
