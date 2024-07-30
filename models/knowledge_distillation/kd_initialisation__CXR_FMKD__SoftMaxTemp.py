3a911364378455b6dd8ef98dd08be8983ec7362b
macuser <fz221@ic.ac.uk>
2024-07-30 13:35:20 +0100

    Corrected val_MSE_loss to val_loss due to training callback using val_loss.

--- models/knowledge_distillation/kd_initialisation__CXR_FMKD__SoftMaxTemp.py
+++ models/knowledge_distillation/kd_initialisation__CXR_FMKD__SoftMaxTemp.py
@@ -26,7 +26,7 @@ from config.config_chexpert import IMAGE_SIZE, CXRFM_EMBEDS_SIZE, EPOCHS, NUM_WO
 from config.config_chexpert import CXRS_FILEPATH, EMBEDDINGS_FILEPATH, TRAIN_RECORDS_CSV, VAL_RECORDS_CSV, MAIN_DIR_PATH
 
 DEV_SPLIT = [0.7, 0.3]
-OUT_DIR_NAME = 'CXR-FMKD_KD-initialisation-MSE/'
+OUT_DIR_NAME = 'CXR-FMKD_KD-initialisation-SoftMaxTemp/'
 
 
 
@@ -177,7 +177,7 @@ def main(hparams):
         default_root_dir=ckpt_dir_path,
         callbacks=[ModelCheckpoint(monitor='val_loss', 
                                    mode='min', 
-                                   filename='best-checkpoint_pre-CXR-FMKD_MSE_{epoch}-{val_loss:.4f}',
+                                   filename='best-checkpoint_pre-CXR-FMKD_SoftMaxTemp_{epoch}-{val_loss:.4f}',
                                    dirpath=ckpt_dir_path), 
                    TQDMProgressBar(refresh_rate=10),
                    train_logger],
--- models/slurm_scripts/cxrfmkd_fft_gpuscript.sh
+++ models/slurm_scripts/cxrfmkd_fft_gpuscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=gpus24       
+#SBATCH --partition=gpus48       
 #SBATCH --gres=gpu:1             
 #SBATCH --output=/vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/slurm_scripts/slurm_logs/cxrfmkd_fft.%N.%j.log  
 #SBATCH --time=3-00:00:00        
