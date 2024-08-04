97f1ba85bd1258c7edb7094b6e9360e8555b98c0
macuser <fz221@ic.ac.uk>
2024-08-04 09:00:46 +0100

    job_name was wrongly named with  --> sixth attempt to solve this, trying with cxrfm_lp.

--- models/multiruns_slurm_scripts/cxrfm_lp_multirun_gpuscript.sh
+++ models/multiruns_slurm_scripts/cxrfm_lp_multirun_gpuscript.sh
@@ -4,9 +4,6 @@
 #SBATCH --output=/vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/multiruns_slurm_scripts/multiruns_slurm_logs/cxrfm_lp/%x.%N.%j.log  
 #SBATCH --time=0-05:00:00        
 
-# Accepts multirun_id from the command line; --job-name replaces the %x in --output above
-#SBATCH --job-name=cxrfm_lp_%1
-
 # Activate the Python virtual environment (fmkd_venv)
 source /vol/bitbucket/fz221/fmkd_venv/bin/activate
 
@@ -18,4 +15,4 @@ export WANDB_CACHE_DIR=/vol/biomedic3/bglocker/mscproj24/fz221/.wandb_storage/ca
 export WANDB_CONFIG_DIR=/vol/biomedic3/bglocker/mscproj24/fz221/.wandb_storage/config
 
 # Run the Python script
-python /vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/disease_prediction__CXR_FM__linear_probing.py --multirun_id "%1"
+python /vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/disease_prediction__CXR_FM__linear_probing.py --multirun_id "$1"
