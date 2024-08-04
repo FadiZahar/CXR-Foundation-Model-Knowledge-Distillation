28f28f4cd4febe640c0b8602bc0ff0ba1bc618b7
macuser <fz221@ic.ac.uk>
2024-08-04 11:43:29 +0100

    job_name was wrongly named with  --> eighth attempt to solve this, trying with cxrfm_lp.

--- models/multiruns_slurm_scripts/cxrfm_lp_multirun_gpuscript.sh
+++ models/multiruns_slurm_scripts/cxrfm_lp_multirun_gpuscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=gpus48       
 #SBATCH --gres=gpu:1             
-#SBATCH --output=/vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/multiruns_slurm_scripts/multiruns_slurm_logs/cxrfm_lp/%x.%N.%j.$1.log  
+#SBATCH --output=/vol/biomedic3/bglocker/mscproj24/fz221/cxr-fmkd/models/multiruns_slurm_scripts/multiruns_slurm_logs/cxrfm_lp/%x.%N.%j.log  
 #SBATCH --time=0-05:00:00        
 
 # Accepts multirun_id from the command line; --job-name replaces the %x in --output above
-#SBATCH --job-name=cxrfm_lp_$1
+#SBATCH --job-name=cxrfm_lp_%1
 
 # Activate the Python virtual environment (fmkd_venv)
 source /vol/bitbucket/fz221/fmkd_venv/bin/activate
