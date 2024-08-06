e94a8adb83030880eb9a25107e2a8ab98ebbe2cf
macuser <fz221@ic.ac.uk>
2024-08-06 19:51:45 +0100

    Added alpha 0.5, 0.6, 0.7, 0.8, 0.9 to config_chexpert.py -- with alpha 0.6 active for testing.

--- config/config_chexpert.py
+++ config/config_chexpert.py
@@ -35,7 +35,11 @@ BEST_CHECKPOINT_KD_MSE_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSE_epoch=38-val
 BEST_CHECKPOINT_KD_CosineSim_FILENAME = 'best-checkpoint_pre-CXR-FMKD_CosineSim_epoch=38-val_loss=0.0106.ckpt'
 BEST_CHECKPOINT_KD_MSEandCosineSim_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSim_epoch=39-val_loss=0.0521.ckpt'
 BEST_CHECKPOINT_KD_MSEandCosineSimLearned_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimLearned_epoch=39-val_loss=-4.8467.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted_alpha0p50_epoch=39_val_loss=1.3008.ckpt'
+# BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted_alpha0p50_epoch=39_val_loss=1.3008.ckpt' # alpha = 0.5
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted_alpha0p60_epoch=38_val_loss=1.2459.ckpt' # alpha = 0.6
+# BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted_alpha0p70_epoch=39_val_loss=1.2486.ckpt' # alpha = 0.7
+# BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted_alpha0p80_epoch=38_val_loss=1.2607.ckpt' # alpha = 0.8
+# BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_FILENAME = 'best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted_alpha0p90_epoch=39_val_loss=1.2506.ckpt' # alpha = 0.9
 
 # Data Paths
 CXRS_FILEPATH = '/vol/biodata/data/chest_xray/CheXpert-v1.0/'
