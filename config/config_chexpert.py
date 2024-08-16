27bb75372c261e4b5f584af0ea1a6fdb8ab50fbb
macuser <fz221@ic.ac.uk>
2024-08-16 03:30:51 +0100

    Updated config_chexpert.py to properly direct to the best checkpoints from the KD inits.

--- config/config_chexpert.py
+++ config/config_chexpert.py
@@ -1,17 +1,17 @@
 # Configuration for CheXpert settings
 
 # Knowledge Distillation
-BEST_CHECKPOINT_KD_MSE_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSE_epoch=38-val_loss=0.0940.ckpt'
-BEST_CHECKPOINT_KD_MAE_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MAE_epoch=39-val_loss=0.2397.ckpt'
-BEST_CHECKPOINT_KD_HuberLoss_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_HuberLoss_epoch=39-val_loss=0.0468.ckpt'
-BEST_CHECKPOINT_KD_CosineSim_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_CosineSim_epoch=38-val_loss=0.0106.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSim_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSim_epoch=38-val_loss=0.0521.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSimLearned_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimLearned_epoch=37-val_loss=-4.8417.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p50_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p50_epoch=38_val_loss=1.3288.ckpt' # alpha = 0.5
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p60_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p60_epoch=39_val_loss=1.2477.ckpt' # alpha = 0.6
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p70_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p70_epoch=38_val_loss=1.2580.ckpt' # alpha = 0.7
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p80_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p80_epoch=39_val_loss=1.2486.ckpt' # alpha = 0.8
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p90_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p90_epoch=39_val_loss=1.2538.ckpt' # alpha = 0.9
+BEST_CHECKPOINT_KD_MSE_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSE_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSE_epoch=38-val_loss=0.0940.ckpt'
+BEST_CHECKPOINT_KD_MAE_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MAE_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MAE_epoch=39-val_loss=0.2395.ckpt'
+BEST_CHECKPOINT_KD_HuberLoss_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-HuberLoss_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_HuberLoss_epoch=38-val_loss=0.0469.ckpt'
+BEST_CHECKPOINT_KD_CosineSim_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-CosineSim_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_CosineSim_epoch=38-val_loss=0.0106.ckpt'
+BEST_CHECKPOINT_KD_MSEandCosineSim_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSim_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSim_epoch=39-val_loss=0.0523.ckpt'
+BEST_CHECKPOINT_KD_MSEandCosineSimLearned_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSimLearned_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimLearned_epoch=38-val_loss=-4.8428.ckpt'
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p50_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSimWeighted-alpha0p50_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p50_epoch=38_val_loss=1.2414.ckpt' # alpha = 0.5
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p60_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSimWeighted-alpha0p60_multirun-seed42/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p60_epoch=39_val_loss=1.2266.ckpt' # alpha = 0.6
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p70_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSimWeighted-alpha0p70_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p70_epoch=38_val_loss=1.2395.ckpt' # alpha = 0.7
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p80_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSimWeighted-alpha0p80_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p80_epoch=38_val_loss=1.2419.ckpt' # alpha = 0.8
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p90_FILENAME = 'multiruns/CheXpert_CXR-FMKD_KD-initialisation-MSEandCosineSimWeighted-alpha0p90_multirun-seed44/lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p90_epoch=38_val_loss=1.2418.ckpt' # alpha = 0.9
 
 # Models
 BEST_CHECKPOINT__CXR_FM_linear_probing__FILENAME = ''
--- config/config_mimic.py
+++ config/config_mimic.py
@@ -1,17 +1,17 @@
 # Configuration for MIMIC-CXR settings
 
 # Knowledge Distillation
-BEST_CHECKPOINT_KD_MSE_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSE_epoch=38-val_loss=0.0940.ckpt'
-BEST_CHECKPOINT_KD_MAE_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MAE_epoch=39-val_loss=0.2397.ckpt'
-BEST_CHECKPOINT_KD_HuberLoss_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_HuberLoss_epoch=39-val_loss=0.0468.ckpt'
-BEST_CHECKPOINT_KD_CosineSim_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_CosineSim_epoch=38-val_loss=0.0106.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSim_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSim_epoch=38-val_loss=0.0521.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSimLearned_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimLearned_epoch=37-val_loss=-4.8417.ckpt'
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p50_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p50_epoch=38_val_loss=1.3288.ckpt' # alpha = 0.5
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p60_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p60_epoch=39_val_loss=1.2477.ckpt' # alpha = 0.6
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p70_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p70_epoch=38_val_loss=1.2580.ckpt' # alpha = 0.7
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p80_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p80_epoch=39_val_loss=1.2486.ckpt' # alpha = 0.8
-BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p90_FILENAME = 'lightning_checkpoints/best-checkpoint_pre-CXR-FMKD_MSEandCosineSimWeighted-alpha0p90_epoch=39_val_loss=1.2538.ckpt' # alpha = 0.9
+BEST_CHECKPOINT_KD_MSE_FILENAME = ''
+BEST_CHECKPOINT_KD_MAE_FILENAME = ''
+BEST_CHECKPOINT_KD_HuberLoss_FILENAME = ''
+BEST_CHECKPOINT_KD_CosineSim_FILENAME = ''
+BEST_CHECKPOINT_KD_MSEandCosineSim_FILENAME = ''
+BEST_CHECKPOINT_KD_MSEandCosineSimLearned_FILENAME = ''
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p50_FILENAME = '' # alpha = 0.5
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p60_FILENAME = '' # alpha = 0.6
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p70_FILENAME = '' # alpha = 0.7
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p80_FILENAME = '' # alpha = 0.8
+BEST_CHECKPOINT_KD_MSEandCosineSimWeighted_alpha0p90_FILENAME = '' # alpha = 0.9
 
 # Models
 BEST_CHECKPOINT__CXR_FM_linear_probing__FILENAME = ''
